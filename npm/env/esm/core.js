e66ab8505e74da1a68a36021293db9db481ec77e
neotales <287371464+neotales@users.noreply.github.com>
2026-09-15 19:41:55 -0500

    chore(env): prepare 0.0.0 release

--- npm/env/esm/core.js
+++ npm/env/esm/core.js
@@ -44,8 +44,9 @@ export const proxy = new Proxy({}, {
     has: (_target, property) => typeof property === "string" && !!property && getEnv(property) !== undefined,
     ownKeys: () => Object.keys(globals.Deno?.env.toObject() ?? globals.process?.env ?? browserEnv),
     getOwnPropertyDescriptor: (_target, property) => {
-        if (typeof property !== "string" || !property || getEnv(property) === undefined)
+        if (typeof property !== "string" || !property || getEnv(property) === undefined) {
             return undefined;
+        }
         return { configurable: true, enumerable: true, value: getEnv(property), writable: true };
     },
 });
